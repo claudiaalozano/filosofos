580d20c3a38bbfc66f258497cea63d2b907bbd60
claudiaalozano <clau.lozano.perez@gmail.com>
2023-05-17 22:23:36 +0200

    commit interfaz

--- interface.py
+++ interface.py
@@ -4,24 +4,7 @@ from filosofos import *
 root= Tk()
 root.geometry("600x600")
 
-#crea una interfaz para mostrar a los filosofos
-class Filosofo:
-    def __init__(self):
-        super().__init__()
-        self.id = filosofo.count
-        filosofo.count += 1
-        filosofo.estado.append('PENSANDO')
-        filosofo.tenedores.append(threading.Semaphore(0))
-        print("Filosofo {} pensando".format(self.id))
-        self.label = Label(root, text="Filosofo {} pensando".format(self.id))
-        self.label.pack()
-    def comer(self):
-        print("Filosofo {} comiendo".format(self.id))
-        time.sleep(random.randint(0,5))
-        print("Filosofo {} termino de comer".format(self.id))
-        self.label.config(text="Filosofo {} comiendo".format(self.id))
-        time.sleep(random.randint(0,5))
-        self.label.config(text="Filosofo {} termino de comer".format(self.id))
-    def __del__(self):
-        print("Filosofo {} se para de la mesa".format(self.id))
-        self.label.config(text="Filosofo {} se para de la mesa".format(self.id))
+tittle = Label(root, text="Cena de los filosofos", font=("Arial", 20)).place(x=150, y=10)
+tittle.config(font=17)
+tittle.pack(pady=2)
+
--- run.py
+++ run.py
@@ -2,5 +2,4 @@ from main import *
 from interface import *
 if __name__ == '__main__':
     main()
-    Filosofo.start()
-    Filosofo.join()
+    
